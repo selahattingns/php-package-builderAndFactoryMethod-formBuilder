f5ea40064c906839ab0b0f20f3bcde3e5df7788d
Selahattin GÜNEŞ <36725371+selahattingunes@users.noreply.github.com>
2023-08-13 20:11:19 +0300

    package usage tested

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+
+$testClass = new SelahattinGns\PhpPackageBuilderPatternFormBuilder\TestClass();
+$testClass->test();
+
---
+++ packages/php-package-builderPattern-formBuilder/src/TestClass.php
@@ -0,0 +1,12 @@
+<?php
+namespace SelahattinGns\PhpPackageBuilderPatternFormBuilder;
+
+class TestClass {
+    /**
+     *
+     */
+    public function test()
+    {
+        echo 123;
+    }
+}
